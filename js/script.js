636c8e04c244445b2b4e0185159bc786d7437b70
BrandonHAnderson <BrandonHAnderson@gmail.com>
2017-01-24 15:23:27 -0600

    Last check, and removed tabs

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function sayHello() {
 
     //Make a variable called message and using the name variable from above,
     // set it to be something like:  "Hello, Mary.  Very nice to meet you."
-    var message = "\tHello, " + name + ".  Very nice to meet you.";
+    var message = "Hello, " + name + ".  Very nice to meet you.";
 
     // Log the message to the console.  Use console.log()
     console.log(message);
@@ -25,7 +25,7 @@ function evenOrOdd() {
     var number = 7
 
     // Write a message (not just the number) to the console saying the value of the number
-    console.log("\tNumber: " + number)
+    console.log("Number: " + number)
 
 
     // Use the modulus operator and an if statement to display a message
@@ -45,7 +45,7 @@ function countToTen() {
     // Write a for loop to count from 1 to 10
     // Use console.log() inside the loop to write out each number
     count = 1
-    while(count <= 10) {
+    while (count <= 10) {
         console.log(count);
         count++
     }
@@ -61,7 +61,7 @@ function addNumbers() {
 
     // Write the array to the console
     count = 0
-    while((count+1) <= numbers.length){
+    while ((count + 1) <= numbers.length) {
         console.log(numbers[count])
         count++
     }
@@ -70,7 +70,7 @@ function addNumbers() {
     var total = 0
 
     // Use a for loop to set total to the sum of all of the numbers in the array
-    for (i of numbers){
+    for (i of numbers) {
         total = total + i;
     }
 
@@ -81,16 +81,14 @@ function addNumbers() {
 
     // Use an if statement to write a message to the console that says
     // whether total is greater than, less than, or equal to 50.
-    if (total < 50){
+    if (total < 50) {
         console.log("Total is less than 50")
-    }
-    else if (total > 50){
+    } else if (total > 50) {
         console.log("Total is greater than 50")
-    }
-    else {
+    } else {
         console.log("Total is equal to 50")
     }
-        
+
 
 }
 
@@ -101,21 +99,21 @@ function addNumbers() {
  * DO NOT MODIFY THE CODE BELOW
  */
 assignmentName = "HW1 - Basic JavaScript"
-console.log( assignmentName );
+console.log(assignmentName);
 console.log();
 
-console.log( "Say Hello" );
+console.log("Say Hello");
 sayHello();
 console.log();
 
-console.log( "Even or Odd" );
+console.log("Even or Odd");
 evenOrOdd();
 console.log();
 
-console.log( "Count to 10" );
+console.log("Count to 10");
 countToTen();
 console.log();
 
-console.log( "Add Numbers" );
+console.log("Add Numbers");
 addNumbers();
 console.log();
