09777682317032ec032a9e3884b147d6ae5d6898
Ryan Anderson <randerson105@users.noreply.github.com>
2017-01-27 19:50:52 -0600

    Created the scripts

--- js/script.js
+++ js/script.js
@@ -4,68 +4,78 @@
  */
 
 function sayHello() {
+    
+    var name = "Ryan";
 
-    // Make a variable called name and set it to your first name.
+    var message = "Hello there my name is " + name + ".";
 
+    var x = console.log(message);
+    
+    return x;
+} 
 
-    //Make a variable called message and using the name variable from above,
-    // set it to be something like:  "Hello, Mary.  Very nice to meet you."
-
-
-    // Log the message to the console.  Use console.log()
-
-
-
-}
 
 function evenOrOdd() {
 
-    // Make a variable called number, and set it to a number between 1 and 10
+    var message1 = "Lets play a game called Even or Odd!";
 
+    var x = console.log(message1);
 
-    // Write a message (not just the number) to the console saying the value of the number
-
-
-    // Use the modulus operator and an if statement to display a message
-    // in the console that says whether the number is even or odd
-
-
-
-
+    var number = Math.floor(Math.random() * 10 + 1);
+    
+    var value = console.log("And the number is " + number + "!");
+    
+        if(number % 2 === 0)
+            {
+                console.log(number + " is an even number (I hope...)")
+            }
+        else
+            {
+                console.log(number + " is an odd number! (or is it...)")
+            }
 }
 
+
 function countToTen() {
-
-    // Write a for loop to count from 1 to 10
-    // Use console.log() inside the loop to write out each number
-
-
-
-
+   
+    for(i=1; i < 11; i++){
+        console.log(i);
+    }
 }
 
 function addNumbers() {
 
     // Make an array of 5 numbers, at least one number should be negative
-
+    var arrayNumb = [-10,34,3,0,5];
 
     // Write the array to the console
+    console.log(arrayNumb);
 
 
     // Make a variable called total, set it to 0
-
+    var total = 0;
 
     // Use a for loop to set total to the sum of all of the numbers in the array
 
-
-
+    for(var i in arrayNumb ) {
+        total += arrayNumb[i];
+    }
 
     // Write a message to the console saying the value of total
-
+    console.log("We add up the numbers to get " + total + " as our total")
 
     // Use an if statement to write a message to the console that says
     // whether total is greater than, less than, or equal to 50.
-
+    if (total > 50){
+        console.log("That is probably bigger than 50...")
+    }
+    else if(total === 50){
+        console.log("That looks like a 50...")
+    }
+    else{
+        console.log("That isn't close to a 50...")
+    }
+        
 
 }
 
@@ -83,7 +93,7 @@ console.log( "Say Hello" );
 sayHello();
 console.log();
 
-console.log( "Even or Odd" );
+console.log();
 evenOrOdd();
 console.log();
 
